218f40795a06c95956e84d3ba510fe807e8fe89c
Eric COURTIAL <e.courtial30@gmail.com>
2020-03-20 20:11:49 +0100

    WIP

--- src/Core/Fs/Ship/ShipExtractor.php
+++ src/Core/Fs/Ship/ShipExtractor.php
@@ -27,9 +27,6 @@ class ShipExtractor
         $filePath = $scenario->getFullPath() . DIRECTORY_SEPARATOR . 'GR.scn';
         $ships = [];
 
-        // Variable for parsing the current entry
-        $mayBe = false;
-
         $entryValues = [
             'NAME' => '',
             'SHORTNAME' => '',
@@ -37,60 +34,85 @@ class ShipExtractor
             'CLASS' => '',
         ];
 
-        // NEED CLEAN AND REFACTO IF WE WANT MORE FIELDS
+        // Variable for parsing the current entry
+        $mayBe = false;
+
         foreach ($this->iniReader->getData($filePath) as $line) {
             // Check the entry type
-            if (array_key_exists($line['key'], $entryValues)) {
-                // Safety to ignore non matching entries (good expected order)
-                if (false === $mayBe && 'NAME' !== $line['key']) {
-                    foreach ($entryValues as &$content) {
-                        $content = '';
-                    }
-                    unset($content);
-
-                    continue;
-                }
-
-                // First expected key
-                if ('NAME' === $line['key']) {
-                    $mayBe = true;
-                }
-
-                $entryValues[$line['key']] = $line['value'];
-
-                // Last expected key
-                if ($mayBe && 'CLASS' === $line['key']) {
-                    // Check that all the var are filled
-                    $allFieldValid = true;
-                    foreach ($entryValues as $content) {
-                        if ('' === $content) {
-                            foreach ($entryValues as &$varContent) {
-                                $varContent = '';
-                            }
-                            unset($varContent);
-                            $allFieldValid = false;
-                            break;
-                        }
-                    }
-
-                    $mayBe = false;
-
-                    if (false === $allFieldValid) {
-                        continue; // Another security
-                    }
-
-                    // Create SHIP here
-                    $ships[] = new Ship($entryValues);
-
-                    // clean variables
-                    foreach ($entryValues as &$varContent) {
-                        $varContent = '';
-                    }
-                    unset($varContent);
-                }
+            if (false === array_key_exists($line['key'], $entryValues)) {
+                continue;
             }
+
+            $this->handleLine($entryValues, $ships, $line, $mayBe);
         }
 
         return $ships;
+    }
+
+    /**
+     * @param string[] $entryValues
+     * @param Ship[] $ships
+     * @param string[] $line
+     * @param bool  $mayBe
+     */
+    protected function handleLine(
+        array &$entryValues,
+        array &$ships,
+        array $line,
+        bool &$mayBe): void
+    {
+        // Safety to ignore non matching entries (good expected order)
+        if (false === $mayBe && 'NAME' !== $line['key']) {
+            $this->cleanVariables($entryValues);
+
+            return;
+        }
+
+        // First expected key
+        if ('NAME' === $line['key']) {
+            $mayBe = true;
+        }
+
+        $entryValues[$line['key']] = $line['value'];
+
+        // Last expected key
+        if ($mayBe && 'CLASS' === $line['key']) {
+            // Check that all the var are filled
+            if (false === $this->validateValues($entryValues)) {
+                return; // Another security
+            }
+
+            $mayBe = false;
+
+            // Create SHIP here
+            $ships[] = new Ship($entryValues);
+
+            // clean variables
+            $this->cleanVariables($entryValues);
+        }
+    }
+
+    /** @param string[] $entryValues */
+    protected function validateValues(array &$entryValues): bool
+    {
+        $allFieldValid = true;
+        foreach ($entryValues as $content) {
+            if ('' === $content) {
+                $this->cleanVariables($entryValues);
+                $allFieldValid = false;
+                break;
+            }
+        }
+
+        return $allFieldValid;
+    }
+
+    /** @param string[] $entryValues */
+    protected function cleanVariables(array &$entryValues): void
+    {
+        foreach ($entryValues as &$varContent) {
+            $varContent = '';
+        }
+        unset($varContent);
     }
 }
